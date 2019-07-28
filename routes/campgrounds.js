f51edad82fadf88b90c2a8f148adf9edafa0fa66
Lucas Val <lucas_val@outlook.com>
2019-07-28 11:11:56 -0300

    Add Author to Campgrounds and Comments

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -11,16 +11,17 @@ router.get("/", function (req, res) {
     });
 });
 
-router.get("/new", function (req, res) {
+router.get("/new", isLoggedIn, function (req, res) {
     res.render("campgrounds/new");
 });
 
-router.post("/", function (req, res) {
+router.post("/", isLoggedIn, function (req, res) {
     var name = req.body.name;
     var image = req.body.image;
     var description = req.body.description;
+    var author = { id: req.user._id, username: req.user.username };
+    var newCampGround = { name: name, image: image, description: description, author: author };
 
-    var newCampGround = { name: name, image: image, description: description };
     Campground.create(newCampGround, function(err, newCamp) {
         if (err)
             console.log(err);
@@ -39,5 +40,13 @@ router.get("/:id", function(req, res) {
             res.render("campgrounds/show", {campground: foundCampground});
     });
 });
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+
+    res.redirect("/login");
+}
 
 module.exports = router;
