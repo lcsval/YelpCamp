8bab0eb66779a14bff75b8efbcbc0c293abea5ee
Lucas Val <lucas_val@outlook.com>
2019-08-03 13:21:38 -0300

    Add mongodb web

--- app.js
+++ app.js
@@ -15,7 +15,16 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds")
     indexRoutes = require("./routes/index")
     
-mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
+//mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://lucasval:1128667@cluster0-uyju3.mongodb.net/test?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useCreateIndex: true
+}).then(() => {
+    console.log("Connected to DB!");
+}).catch(err => {
+    console.log("ERROR: ", err.message);
+});
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
